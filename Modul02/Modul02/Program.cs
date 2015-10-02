e1a9aea0bd7748aafb3864edab928da29036acb9
гÐTeodora Hristowa <teo_hristova@abv.bg>
2015-10-02 11:46:22 +0300

    Модул 02, Видео 01, Добавяне на комент

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзола
+		}
+	}
+}
