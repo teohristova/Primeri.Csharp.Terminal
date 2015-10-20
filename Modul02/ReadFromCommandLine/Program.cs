f0b9ab42f87f9b66ba66950f7baf357cd60ff4cc
гÐTeodora Hristowa <teo_hristova@abv.bg>
2015-10-20 13:51:22 +0300

    ÐM02, V05, Chetene ot red

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+			int a = 5, b = 0;
+			//Vavejdane na vhodni parametri
+			Console.Write ("Molq vavedeta a=");
+			b = Convert.ToInt32 (Console.ReadLine ());
+			// Pechat na rezultat
+			Console.WriteLine ("a+b="+(a+b).ToString()+"\n\n\n");
+
+		}
+	}
+}
