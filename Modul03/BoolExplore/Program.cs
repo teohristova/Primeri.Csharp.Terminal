f06acab795e4cbcc8f2f4b5dfb8d31494e359ba8
гÐTeodora Hristowa <teo_hristova@abv.bg>
2015-11-06 16:26:02 +0200

    Модул03, Видео05-Булеви опеации

---
+++ Modul03/BoolExplore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BoolExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int [] _i = new int[] { 4, 3, 2 };
+			int _iu = Convert.ToInt32 (Console.ReadLine ( ));
+			bool _chek01 = (_iu <= _i.Length);
+			bool _chek02 = (_iu >0);
+			bool _exp01 = _chek01 && _chek02; // Логическо и ; "&&''
+			bool _exp02 = _chek01 || _chek02; // Логическо и ; "||''
+			bool _exp03 = !_chek01 ; // Логическо отрицание ; "!''
+			
+			Console.WriteLine ("/nЛогическо И :   " + _exp01.ToString());
+			Console.WriteLine ("/nЛогическо ИЛИ :  " + _exp02.ToString());
+			Console.WriteLine ("/nОтрицание на " +  _chek01.ToString() + ":"+ _exp03.ToString());
+			Console.ReadKey ();
+
+		}
+	}
+}
--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -6,15 +6,22 @@ namespace Modul03
 	{
 		public static void Main (string[] args)
 		{
-			double a = 0, b = 0;
+			double a = 0, b = 0, c=0 ;
 			Console.Write (">a = ");
 			a = Convert.ToDouble (Console.ReadLine ());
 			Console.Write (">b = ");
 			b = Convert.ToDouble (Console.ReadLine());
+			// Форматиране на дименсия
 			string _format = " #0.00m3 ";
-
-			Console.WriteLine ("\n\n> a/b=" + (a / b).ToString(  _format ));
+			//Степенуване на числа
+			c= Math.Pow (a,b);
+			Console.WriteLine ("\n\n> a^b=" + (c).ToString(  _format ));
+			//Коренуване на числа
+			c= Math.Sqrt (b);
+			Console.WriteLine ("\n\n> b^1/2=" + (c).ToString(  ));
+			//Степенуване на числа
+			c=Math.Round(a/b);
+			Console.WriteLine ("\n\n> a/b=" + (c));
 		}
 	}
 }
- 
