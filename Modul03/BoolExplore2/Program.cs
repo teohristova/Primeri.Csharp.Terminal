fd6f633303db52009ceb147d8fd3e4b8ecd6c148
гÐTeodora Hristowa <teo_hristova@abv.bg>
2015-11-06 22:38:44 +0200

    Модул03, Видео09, ОпераÐтор Switch

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -12,9 +12,35 @@ namespace BoolExplore2
 			Console.WriteLine ( "\nВъведете индекс :  ");
 			_input = Console.ReadLine ();
 			bool _check01=int.TryParse (_input, out _iu);
-			_iu = (_iu <= _i.Length) ? _iu : 3;
-			_iu = (_iu >0) ? _iu : 1;
-			 Console.WriteLine ( _i[_iu-1]);
+//			_iu = (_iu <= _i.Length) ? _iu : 3;
+//			_iu = (_iu >0) ? _iu : 1;
+			switch (_iu) {
+			case 1 : 
+				{
+					Console.WriteLine ( "1-ви елемент на масива е:");	
+			 		Console.WriteLine ( _i[_iu-1]);
+					break;
+				}
+			case 2:   
+				{
+					Console.WriteLine ( "2-ри елемент на масива е:");	
+					Console.WriteLine ( _i[_iu-1]);
+					break;
+				}
+			case 3:   
+				{
+					Console.WriteLine ( "3-ти елемент на масива е:");	
+					Console.WriteLine ( _i[_iu-1]);
+					break;
+				}
+			default : 
+				
+				{ 
+					Console.WriteLine ("В масива няма елемент с този индекс. \n\n\nМоля изберете индекс между 1 и 3");
+					break;
+				}
+					
+			}
 			Console.ReadKey ();
 		}
 	}
