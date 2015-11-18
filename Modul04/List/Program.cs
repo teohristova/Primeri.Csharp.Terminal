9624d30bb6e962e847ba95975fc0a72ef26f7c1f
гÐTeodora Hristowa <teo_hristova@abv.bg>
2015-11-18 16:17:45 +0200

    Mодул 04, Примери с LIST

---
+++ Modul04/List/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ListExplore
+
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//defenicii
+			string  userinput = " ";
+			List <int> _list= new List <int> ();
+			_list.Add  (5);
+			_list.Add (3);
+			_list.Add (9);
+			//vhod
+			do {
+				Console.Write ("?");
+				userinput = Console.ReadLine ();
+				//add valves
+				if (userinput.ToLower (). Contains ("add"))
+				{
+					try 
+					{
+						int _add = 0;
+						if (int.TryParse (userinput.Split (' ') [1], out _add)) {
+							_list.Add (_add);
+						}
+					} catch {
+					}
+				}
+
+				//review
+				if (userinput.Contains ("show")) {
+					Console.Write ("list:");
+					for (int i = 0; i < _list.Count; i++) {
+						Console.Write (_list [i]);
+						if (i < _list.Count - 1)
+							Console.Write (",");
+					}
+					Console.WriteLine ();
+				
+			
+			}
+				//size
+			if (userinput.ToLower (). Contains ("size"))
+			{
+				Console.WriteLine ( "Дължината на списъкът е:" + _list.Count.ToString()+"\n");
+			}
+			}
+			while (userinput != "exit");
+				
+		}
+	}
+}
+
+
+	
+
+
+
+
